0f1cea31c908a630c8f6383768cb062a69c069c0
alvo254 <alvinkhamron@gmail.con>
2022-10-19 10:53:11 +0300

    login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+import {   BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login/Login';
+import Signup from "./components/SignUp/SignUp"
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
       
     </div>
   );
