c9b3826fc23ecc87ddf76d16b299e6972a4a5f9b
Zamzam <Zamzamhassan44@gmail.com>
2022-10-19 14:17:19 +0300

    added styling to landing page-hoem

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,18 @@
+import React from "react"
+import "./Home.css"
+
+
 function Home(){
 return<>
 <div className="home">
-<h1>We Are Top Courier <span>and Mover Service</span>in Nairobi</h1>
-<p>An application where users can login to send packages.Just like Wells Fargo, Sendy etc.The application charge users</p>
+<h1 className="caption">We Are Top Courier and Mover Service in Nairobi</h1>
+<p className="first">An application where users can login to send packages.Just like Wells Fargo, Sendy etc.The application charge users</p>
 <button>Our Solutions</button>
 
-<h2>55+ Towns Covered</h2>
-<h2>1000+ Satisfied Clients</h2>
-<img src="" alt=""/>
+<h2 className="town">55+ Towns Covered</h2>
+<h2 className="client">1000+ Satisfied Clients</h2>
+<img src="https://i.pinimg.com/564x/75/01/d5/7501d56f7ac90e6cdd21df70256b43ff.jpg" alt="bike man"/>
+<img src="https://i.pinimg.com/236x/f2/51/84/f25184a196a1e00e6a5fcbd2439d770e.jpg" alt="bike man" />
 
 </div>
 
