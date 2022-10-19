737c6a89cf16fd038eee4a329472f9070d582fa2
alvo254 <alvinkhamron@gmail.con>
2022-10-19 14:37:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import {   BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {   BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+import { verify } from "./auth/Users";
 import Login from './components/Login/Login';
 import Signup from "./components/SignUp/SignUp"
 
 function App() {
-  return (
-    <div className="App">
+  
 
-      <BrowserRouter>
-        <Routes>
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Signup' element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
-      
-    </div>
+  const [authorized, setAuthorized] = useState(null);
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const reverify = async () => {
+      try {
+        const currUser = await verify();
+        setUserData(currUser);
+        setAuthorized(true);
+      } catch (error) {
+        setAuthorized(false);
+      }
+    };
+    reverify();
+  }, []);
+
+  return authorized === true || authorized === false ? (
+    <div className="App">
+    <BrowserRouter>
+      <Routes>
+      <Route path='/Login' element={<Login authorized={authorized} setUserData={setUserData} />} />
+      <Route path='/Signup' element={<Signup authorized={authorized} setUserData={setUserData} />} />
+      {authorized ? (
+        <></>
+      ) : (
+        <Navigate to="/login"/>
+      )}
+    </Routes>
+  </BrowserRouter>
+    
+  </div>
+
+  ) : (
+    <h1>Loading</h1>
   );
+
 }
 
 export default App;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { login } from '../../auth/Users';
 
-export default function Login() {
+export default function Login({ authorized, setUserData }) {
+
+    // const history = useHistory()
+
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+        is_admin: false,
+      });
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser({
+          ...user,
+          [name]: value,
+        });
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const createUser = async () => {
+          const newUser = await login(user);
+          setUserData(newUser);
+          setTimeout(() => {
+            // history.push("/");
+          }, 500);
+        };
+        createUser();
+      };
+
+
+
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
                 <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
                    Sign in
                 </h1>
-                <form className="mt-6">
+                <form onSubmit={handleSubmit} className="mt-6">
                     <div className="mb-2">
                         <label
                             for="email"
@@ -17,6 +50,9 @@ export default function Login() {
                             Email
                         </label>
                         <input
+                            name='email'
+                            value={user.email}
+                            onChange={handleChange}
                             type="email"
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
@@ -29,6 +65,9 @@ export default function Login() {
                             Password
                         </label>
                         <input
+                            name='password'
+                            onChange={handleChange}
+                            value={user.password}
                             type="password"
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
@@ -40,7 +79,7 @@ export default function Login() {
                         Forget Password?
                     </a>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                        <button type='submit' className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                             Login
                         </button>
                     </div>
