68d5a46e14e500921fb6d210d7ef7b270fa2ad1a
Newton Ombese <newtonombese1@gmail.com>
2022-10-19 10:23:32 +0300

    working on the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <h1>Hello</h1>
     </div>
   );
 }
