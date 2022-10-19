b32707942dbc9feaf6aba2701f1c8f266c105d84
Zamzam <Zamzamhassan44@gmail.com>
2022-10-19 12:54:20 +0300

    added home rout to app.js

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,15 @@
 function Home(){
 return<>
 <div className="home">
-<P>We Are Top Courier <span>and Mover Service</span>in Nairobi</P></div>
+<h1>We Are Top Courier <span>and Mover Service</span>in Nairobi</h1>
+<p>An application where users can login to send packages.Just like Wells Fargo, Sendy etc.The application charge users</p>
+<button>Our Solutions</button>
+
+<h2>55+ Towns Covered</h2>
+<h2>1000+ Satisfied Clients</h2>
+<img src="" alt=""/>
+
+</div>
 
 
 </>
