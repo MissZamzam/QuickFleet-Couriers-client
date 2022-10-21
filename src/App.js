8c6ce32d0da9a5fbc55e2f6d53adf23d441a1c3b
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-21 12:28:21 +0300

    receipts  component and route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Home from './components/Home/Home';
 import Deliveries from './components/Delivery/Deliveries';
  import Delivery from './components/Delivery/Delivery';
  import Receipts from './components/Receipts/Receipts';
-// import Receipt from './components/Receipts/Receipt';
+ import Receipt from './components/Receipts/Receipt';
 // import Trackings from './components/Tracking/Trackings';
 // import Tracking from './components/Tracking/Trackings';
 
@@ -28,7 +28,7 @@ function App() {
         <Route path='/deliveries' element={<Deliveries />} />
         <Route path='/delivery/:id' element={<Delivery />} />
         <Route path='/receipts' element={<Receipts />} /> 
-        {/* <Route path='/receipt/:id' element={<Receipt />} />  */}
+        <Route path='/receipt/:id' element={<Receipt />} /> 
         {/* <Route path='/trackings' element={<Trackings />} />
         <Route path='/tracking/:id' element={<Tracking />} /> */}
       </Routes>
