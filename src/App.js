531742f1f536ccc3dbb3e4804eb263c199f43ad2
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-21 12:29:44 +0300

    receipts  component and route

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home';
 import Deliveries from './components/Delivery/Deliveries';
- import Delivery from './components/Delivery/Delivery';
- import Receipts from './components/Receipts/Receipts';
- import Receipt from './components/Receipts/Receipt';
+import Delivery from './components/Delivery/Delivery';
+import Receipts from './components/Receipts/Receipts';
+import Receipt from './components/Receipts/Receipt';
 // import Trackings from './components/Tracking/Trackings';
 // import Tracking from './components/Tracking/Trackings';
 
@@ -29,8 +29,8 @@ function App() {
         <Route path='/delivery/:id' element={<Delivery />} />
         <Route path='/receipts' element={<Receipts />} /> 
         <Route path='/receipt/:id' element={<Receipt />} /> 
-        {/* <Route path='/trackings' element={<Trackings />} />
-        <Route path='/tracking/:id' element={<Tracking />} /> */}
+        {/* <Route path='/trackings' element={<Trackings />} /> */}
+        {/* <Route path='/tracking/:id' element={<Tracking />} /> */}
       </Routes>
     </BrowserRouter>
       
