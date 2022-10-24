6dee68f72658ca506b85e09ae26cf5f4c6118fb7
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-24 08:54:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { useEffect, useState } from 'react';
-import {   BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
-import React from 'react'
-import {   BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { verify } from "./auth/Users";
 import Login from './components/Login/Login';
 import Signup from "./components/SignUp/SignUp"
+
+import './App.css';
+import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import DeliveryForm from './components/DeliveryDetails/DeliveryForm'
-// import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home';
 import Deliveries from './components/Delivery/Deliveries';
 import Delivery from './components/Delivery/Delivery';
@@ -19,42 +16,8 @@ import Trackings from './components/Tracking/Trackings';
 import Tracking from './components/Tracking/Trackings';
 
 function App() {
-  
-
-  const [authorized, setAuthorized] = useState(null);
-  const [userData, setUserData] = useState({});
-
-  useEffect(() => {
-    const reverify = async () => {
-      try {
-        const currUser = await verify();
-        setUserData(currUser);
-        setAuthorized(true);
-      } catch (error) {
-        setAuthorized(false);
-      }
-    };
-    reverify();
-  }, []);
-
-  return authorized === true || authorized === false ? (
+  return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-      <Route path='/Login' element={<Login authorized={authorized} setUserData={setUserData} />} />
-      <Route path='/Signup' element={<Signup authorized={authorized} setUserData={setUserData} />} />
-      {authorized ? (
-        <></>
-      ) : (
-        <Navigate to="/login"/>
-      )}
-    </Routes>
-  </BrowserRouter>
-    
-  </div>
-
-  ) : (
-    <h1>Loading</h1>
       <BrowserRouter>
       <Navbar />
       <Home />
@@ -70,13 +33,10 @@ function App() {
         <Route path='/tracking/:id' element={<Tracking />} />
       </Routes>
     </BrowserRouter>
-      <Navbar />
-      <DeliveryForm />
       
      
     </div>
   );
-
 }
 
 export default App;
