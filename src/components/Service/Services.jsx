268d240fac6536100909df7ea974e4430d049361
Zamzam <Zamzamhassan44@gmail.com>
2022-10-24 15:08:53 +0300

    fetched and displayed services from the backend to frontend service page

--- src/components/Service/Services.jsx
+++ src/components/Service/Services.jsx
@@ -11,33 +11,41 @@ export default function Services() {
     const [services, setServices] = useState([])
 
     useEffect(()=>{
-        fetch("")
+        fetch("http://127.0.0.1:3000/deliveries")
         .then((res) => res.json()) 
         .then((data)=>setServices(data))
     }, [])
 
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      {/* <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-    </Card>
+<div class="grid gap-10 px-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">    
+    {services.map((service)=>{
+            return(
+                
+                <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={service.image_url}
+                />
+                {/* <img src={service.image_url} /> */}
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {service.category}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {service.description}
+                </Typography>
+                </CardContent>
+                {/* <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+                </CardActions> */}
+            </Card>
+            )
+        })}
+    </div>
+   
   );
 }
