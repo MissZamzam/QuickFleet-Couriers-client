943505b8d2c64c28c947c13d86ec6d345a955f22
Zamzam <Zamzamhassan44@gmail.com>
2022-10-24 11:52:06 +0300

    imported useState, useState from react and container from bootstrap

--- src/components/Delivery/Deliveries.jsx
+++ src/components/Delivery/Deliveries.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import React, { useEffect, useState } from 'react'
+import {  Container } from "react-bootstrap";
+
 import './Deliveries.css'
 
 function Deliveries () {
