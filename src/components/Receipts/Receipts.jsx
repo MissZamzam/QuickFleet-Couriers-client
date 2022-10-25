11706ffc7cd06adeaddd4322e0a70653d214080e
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-25 11:37:11 +0300

    add view button

--- src/components/Receipts/Receipts.jsx
+++ src/components/Receipts/Receipts.jsx
@@ -1,80 +1,62 @@
 import { useState, useEffect } from "react";
 import * as React from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { Link } from "react-router-dom";
-import Receipt from "./Receipt"
+import Receipt from "./Receipt";
 import { Box } from "@mui/material";
 
 import "./Receipts.css";
 
 function Receipts() {
-    const [ receipts, setReceipts ] = useState( [] );
-    const columns = [
-      { field: "id", headerName: "ID", flex: 0.5 },
-      { field: "receipt_no", headerName: "Receipt.No" },
-      {
-        field: "sender_name",
-        headerName: "Sender Name",
-        flex: 1,
-        cellClassName: "name-column--cell",
+  const [receipts, setReceipts] = useState([]);
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "receipt_no", headerName: "Receipt.No" },
+    {
+      field: "sender_name",
+      headerName: "Sender Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "receiver_name",
+      headerName: "Receiver Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "nature_of_goods",
+      headerName: "Nature Of Goods",
+      flex: 1,
+    },
+    {
+      field: "pickup",
+      headerName: "PickUp",
+      flex: 1,
+    },
+    {
+      field: "destination",
+      headerName: "Destination",
+      flex: 1,
+    },
+    {
+      field: "amount_paid",
+      headerName: "Amount Paid",
+      flex: 1,
       },
-      {
-        field: "receiver_name",
-        headerName: "Receiver Name",
-        type: "string",
-        headerAlign: "center",
-        align: "center",
-      },
-      {
-        field: "nature_of_goods",
-        headerName: "Nature Of Goods",
-        flex: 1,
-      },
-      {
-        field: "pickup",
-        headerName: "PickUp",
-        flex: 1,
-      },
-      {
-        field: "destination",
-        headerName: "Destination",
-        flex: 1,
-      },
-      {
-        field: "amount_paid",
-        headerName: "Amount Paid",
-        flex: 1,
-      },
-      {
-        field: "view",
-        headerName: "View",
-        flex: 1,
-      },
-    ];
-
+  ];
 
   useEffect(() => {
     fetch("http://127.0.0.1:3000/receipts")
       .then((response) => response.json())
       //   .then( ( response ) => console.log(response) )
       .then((data) => {
-          console.log( data );
-          setReceipts(data)
+        console.log(data);
+        setReceipts(data);
       });
   }, []);
-    return (
-         <Box m="20px">
-      {/* <Header
-        title="CONTACTS"
-        subtitle="List of Contacts for Future Reference"
-      /> */}
+  return (
+    <Box m="20px">
       <Box
         m="40px 0 0 0"
         height="75vh"
@@ -85,26 +67,26 @@ function Receipts() {
           "& .MuiDataGrid-cell": {
             borderBottom: "none",
           },
-        //   "& .name-column--cell": {
-        //     color: colors.greenAccent[300],
-        //   },
-        //   "& .MuiDataGrid-columnHeaders": {
-        //     backgroundColor: colors.blueAccent[700],
-        //     borderBottom: "none",
-        //   },
-        //   "& .MuiDataGrid-virtualScroller": {
-        //     backgroundColor: colors.primary[400],
-        //   },
-        //   "& .MuiDataGrid-footerContainer": {
-        //     borderTop: "none",
-        //     backgroundColor: colors.blueAccent[700],
-        //   },
-        //   "& .MuiCheckbox-root": {
-        //     color: `${colors.greenAccent[200]} !important`,
-        //   },
-        //   "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-        //     color: `${colors.grey[100]} !important`,
-        //   },
+          //   "& .name-column--cell": {
+          //     color: colors.greenAccent[300],
+          //   },
+          //   "& .MuiDataGrid-columnHeaders": {
+          //     backgroundColor: colors.blueAccent[700],
+          //     borderBottom: "none",
+          //   },
+          //   "& .MuiDataGrid-virtualScroller": {
+          //     backgroundColor: colors.primary[400],
+          //   },
+          //   "& .MuiDataGrid-footerContainer": {
+          //     borderTop: "none",
+          //     backgroundColor: colors.blueAccent[700],
+          //   },
+          //   "& .MuiCheckbox-root": {
+          //     color: `${colors.greenAccent[200]} !important`,
+          //   },
+          //   "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+          //     color: `${colors.grey[100]} !important`,
+          //   },
         }}
       >
         <DataGrid
@@ -113,40 +95,11 @@ function Receipts() {
           components={{ Toolbar: GridToolbar }}
         />
       </Box>
-    </Box>      
-    );
+    </Box>
+  );
 }
 export default Receipts;
 
-
-
-{
-  /* <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell align="right">Receipt.No</TableCell>
-                <TableCell align="right">Sender Name</TableCell>
-                <TableCell align="right">Receiver Name</TableCell>
-                <TableCell align="right">Nature Of Goods</TableCell>
-                <TableCell align="right">PickUp</TableCell>
-                <TableCell align="right">Destination</TableCell>
-                <TableCell align="right">Amount Paid</TableCell>
-                {/* <TableCell align="right">Delivery Id</TableCell> */
-}
-{
-  /* <TableCell align="right">View</TableCell> */
-}
-{
-  /* <TableCell align="right">Delete</TableCell> */
-}
-{
-  /* </TableRow> */
-}
-{
-  /* </TableHead> */
-}
 {
   /* <TableBody>
               {receipts.map((row) => (
@@ -182,4 +135,4 @@ export default Receipts;
 }
 {
   /* </TableContainer> */
-} 
+}
