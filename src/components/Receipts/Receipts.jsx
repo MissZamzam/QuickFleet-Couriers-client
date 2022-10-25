339498f1bccca7186146c182f298a86b79bb890e
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-25 21:13:34 +0300

    incorporate receipts into admin dashboard

--- src/components/Receipts/Receipts.jsx
+++ src/components/Receipts/Receipts.jsx
@@ -9,7 +9,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Link } from "react-router-dom";
-import Receipt from "./Receipt"
+// import Receipt from "./Receipt"
 import { Box } from "@mui/material";
 
 import "./Receipts.css";
