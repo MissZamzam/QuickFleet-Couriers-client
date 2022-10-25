5a3c8e981cb2b66e9f6c485ce37b45f495422aec
Newton Ombese <newtonombese1@gmail.com>
2022-10-25 12:14:55 +0300

    finishing on the order form

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import FormLabel from '@mui/material/FormLabel'
+import "./OrderForm.css"
 
-function OrderForm() {
+export default function OrderForm() {
   return (
-    <div>
-        <h1>Orders</h1>
+    <div className='form'>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        style={{"background":"white",
+        "padding":'20px',
+        "marginTop":"40px",
+        "width":'41%',
+        }}
+      >
+      <FormLabel
+        style={{"justifyContent": 'center',
+          "alignItems":"center",
+          "display": "flex"
+        }}
+      >
+        Place your order
+      </FormLabel>
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Sender name"
+          />
+          <TextField
+            id="outlined-disabled"
+            label="Receiver name"
+          />
+          <div>
+            <TextField
+              id="outlined-password-input"
+              label="Nature of goods"
+            />
+            <TextField
+              id="outlined-read-only-input"
+              label="Amount paid"
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-password-input"
+              label="Pickup"
+            />
+            <TextField
+              id="outlined-read-only-input"
+              label="Destination"
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-password-input"
+              label="Delivery Id"
+            />
+            <Button variant="contained">Make Order</Button>
+          </div>
+        </div>
+      </Box>
     </div>
-  )
+  );
 }
 
-export default OrderForm
+
+
+
+
