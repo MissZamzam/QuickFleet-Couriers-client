58c368ea338a596074b55f3b2cd1a713296b4ba8
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-27 01:54:34 +0300

    dashboard

--- src/Dashboard/scenes/Deliveries.jsx
+++ src/Dashboard/scenes/Deliveries.jsx
@@ -190,13 +190,20 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button } from "@mui/material";
+import { Button, Stack} from "@mui/material";
 import { Link }from 'react-router-dom';
-import Editdelivery from "./Editdelivery";
+import Delivery from './Delivery';
+import "./Deliveries.css"
+// import Editdelivery from "./Editdelivery";
 
 
 function Deliveries() {
+  
+  
+
+  
 const[deliveries, setDeliveries]= useState([])
+
 
 useEffect(()=>{
     fetch("http://127.0.0.1:3000/deliveries")
@@ -266,12 +273,28 @@ return(
               {/* <TableCell align="right">{row.delivery_id}</TableCell>
               <TableCell align="right">{row.receipt_id}</TableCell> */}
               <TableCell align="right"><Link to="/deliveries/:id">{row.view}</Link></TableCell>
+              {/* <Stack direction="row" spacing={2}>
+            <Link
+              to={`/deliveries/${id}`}
+              onClick={() => <Delivery key={deliveries.id} />}
+            >
+              <button className="viewBtn">View</button>
+            </Link>
+            <Button
+              variant="outlined"
+              color="error"
+              size="small"
+              onClick={onClick}
+            >
+              Delete
+            </Button>
+          </Stack> */}
               
               {/* <button onClick={() => {deleteDelivery (row.id)}} type="button display in-line padding: 15px" className="btn-danger btn-xsm">DELETE</button> */}
-              <Button  element={<Editdelivery/>}variant="contained" color="success">EDIT</Button>
+             <Button classname="editbtn" variant="contained" color="success">EDIT</Button>
           
               <Button onClick={() => {deleteDelivery (row.id)}} variant="outlined" color="error">DELETE</Button>
-            
+             
           
             </TableRow>
           ))}
