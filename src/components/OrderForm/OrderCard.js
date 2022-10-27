6e648e60aefc55c9b6e5ba79c4f63b808d17c4b4
Newton Ombese <newtonombese1@gmail.com>
2022-10-27 10:59:44 +0300

    working on the responsiveness of the footer and home page

--- src/components/OrderForm/OrderCard.js
+++ src/components/OrderForm/OrderCard.js
@@ -1,36 +1,3 @@
-// import React, {useEffect, useState} from 'react'
-
-// function OrderCard() {
-    // const [orders, setOrders] = useState([])
-
-    // useEffect(() =>{
-    //     fetch('http://localhost:3000/orders')
-    //     .then(res => res.json())
-    //     .then(data => setOrders(data))
-    // }, [])
-
-    // function handleDelete(order){
-    //     fetch(`http://localhost:3000/orders/${order.id}`,{
-    //         method: 'DELETE'
-    //     })
-    // }
-//   return (
-//     <div>
-//         {
-//             orders.map((order)=>{
-//                 return(
-//                     <div>
-
-//                     </div>
-//                 )
-//             })
-//         }
-//     </div>
-//   )
-// }
-
-// export default OrderCard
-
 import * as React from 'react';
 import {useEffect, useState} from 'react'
 import Card from '@mui/material/Card';
