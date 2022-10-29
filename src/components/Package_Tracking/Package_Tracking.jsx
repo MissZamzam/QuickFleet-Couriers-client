aac21278c59b4b45978293c710caafe4d54b0288
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-29 23:40:13 +0300

    add close icon to search bar

--- src/components/Package_Tracking/Package_Tracking.jsx
+++ src/components/Package_Tracking/Package_Tracking.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
-import Receipts from "../Receipts/Receipts";
 import "./Package_Tracking.css";
 
 function Package_Tracking() {
   const [ receipts, setReceipts ] = useState( [] );
   const [filteredData, setFilteredData] = useState([]);
-  // const [wordEntered, setWordEntered] = useState("");
-  // const [searchReceipts, setSearchReceipts] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
 
   useEffect( () =>
   {
@@ -39,9 +37,17 @@ function Package_Tracking() {
       console.log(newFilter);
     }
 
-    // setFilteredData( newFilter );
-   
+      const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+      };
+
+    // setFilteredData( newFilter ); 
   }
+    const clearInput = () => {
+      setFilteredData([]);
+      setWordEntered("");
+    };
   
   return (
     <div className="container-fluid bg">
@@ -60,20 +66,23 @@ function Package_Tracking() {
                 // value={receipt_no}
               />
               <div className="searchIcon">
-                <SearchIcon/> 
+                {filteredData.length === 0 ? (
+                  <SearchIcon />
+                ) : (
+                  <CloseIcon id="clearBtn" onClick={clearInput} />
+                )}
+              </div>
             </div>
-            </div>
-            { filteredData.length != 0 && (
+            {filteredData.length != 0 && (
               <div className="dataResults">
-                { filteredData.slice(0, 3).map( ( receipt, index ) =>
-                {
+                {filteredData.slice(0, 3).map((receipt, index) => {
                   return (
                     <a className="dataItem" href="/receipts" target="blank">
-                      {/* { receipt.receipt_no } */ }
-                      <p>{ receipt.receipt_no }</p>
+                      {/* { receipt.receipt_no } */}
+                      <p>{receipt.receipt_no}</p>
                     </a>
                   );
-                } ) }
+                })}
               </div>
             )}
 
@@ -96,144 +105,6 @@ function Package_Tracking() {
 }
 
 export default Package_Tracking;
-
-// import React, { useState, useEffect } from "react";
-// import SearchIcon from "@mui/icons-material/Search";
-// import CloseIcon from "@mui/icons-material/Close";
-// import "./Package_Tracking.css";
-
-// function Package_Tracking({receipts}) {
-//   const [receipt_no, setReceiptNumber] = useState("");
-//   // const [singleReceipt, setSingleReceipt] = useState({
-//   //   receipt_no: "",
-//   //   sender_name: "",
-//   //   receiver_name: "",
-//   //   nature_of_goods: "",
-//   //   pickup: "",
-//   //   destination: "",
-//   //   amount_paid: 0,
-//   // });
-
-//   const [filteredData, setFilteredData] = useState([]);
-//   const [wordEntered, setWordEntered] = useState("");
-//   const [{ data: receipt, error, status }, setReceipt] = useState({
-//     data: {},
-//     error: "",
-//     status: "pending",
-//   });
-//   // console.log(singleReceipt.receipt_no);
-
-//   useEffect(() => {
-//     fetch("http://127.0.0.1:3000/receipts").then((response) => {
-//       if (response.ok) {
-//         response.json().then((receipts) => {
-//           console.log(receipts);
-
-//           // receipts.forEach((receipt) => {
-//           //   if (receipt.receipt_no === receipt_no) {
-//           //     // console.log(receipt);
-//           //     setSingleReceipt({
-//           //       receipt_no: receipt.receipt_no,
-//           //       sender_name: receipt.sender_name,
-//           //       receiver_name: receipt.receiver_name,
-//           //       nature_of_goods: receipt.nature_of_goods,
-//           //       pickup: receipt.pickup,
-//           //       destination: receipt.destination,
-//           //       amount_paid: receipt.amount_paid,
-//           //     });
-//           //   }
-//           // });
-//           // setReceipts([...receipts, receipt])
-//           setReceipt({ data: receipt, error: "", status: "resolved" });
-//         });
-//       } else {
-//         response.json().then((err) =>
-//           setReceipt({
-//             data: "not found",
-//             error: err.error,
-//             status: "rejected",
-//           })
-//         );
-//       }
-//     });
-//   }, [receipt_no]);
-
-// const handleFilter = (event) => {
-//   const searchWord = event.target.value;
-//   setWordEntered( searchWord );
-
-//   const newFilter = receipts?.filter( ( receipt ) =>
-//   {
-//     console.log(receipt);
-//     return receipt.receipt_no.toLowerCase().includes( searchWord.toLowerCase() );
-//   } );
-
-//   if (searchWord === "") {
-//     setFilteredData([]);
-//   } else {
-//     setFilteredData(newFilter);
-//   }
-// };
-//   const clearInput = () => {
-//     setFilteredData([]);
-//     setWordEntered("");
-//   };
-
-//   return (
-//     <div className="container-fluid bg">
-//       <div className="receipt-form">
-//         <form className="receiptNo-form">
-//           <h5>Track Your Package Delivery </h5>
-//           <p>Enter the receipt number that you got from our offices</p>
-//           <div className="form-group">
-//             <input
-//               type="text"
-//               id="receipt_no"
-//               autoComplete="off"
-//               className="form-control"
-//               placeholder="enter your receipt.no..."
-//               value={wordEntered}
-//               onChange={handleFilter}
-//             />
-//             <div className="searchIcon">
-//               {filteredData?.length === 0 ? (
-//                 <SearchIcon key={receipt.id}/>
-//               ) : (
-//                 <CloseIcon id="clearBtn" onClick={clearInput} />
-//               )}
-//               {/* <i className="fa fa-search searchIcon" aria-hidden="true"></i> */}
-//             </div>
-//           </div>
-//           {filteredData?.length != 0 && (
-//             <div className="dataResult">
-//               { filteredData?.slice( 0, 15 ).map( ( receipt ) =>
-//               {
-//                 console.log(receipt);
-//                 return (
-//                   <a className="dataItem" href={receipt.link} target="blank">
-//                     <p>{receipt.receipt_no} </p>
-//                   </a>
-//                 );
-//               })}
-//             </div>
-//           )}
-//           <div className=" container package-receipt">
-//             <div className="receipt-details">
-//               {/* <h5>{singleReceipt.receipt_no}</h5> */}
-//               <h5>Quick Fleet</h5>
-//               <p>#KEN23454634</p>
-//             </div>
-//             <div>
-//               <button className="receiptBtn">On Process</button>
-//             </div>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Package_Tracking;
 
 // import React, { useState, useEffect } from 'react';
 // import SearchIcon from "@mui/icons-material/Search";
