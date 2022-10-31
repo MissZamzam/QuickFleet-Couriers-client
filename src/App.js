0749b54c3b274f750132714605444074eda04509
Newton Ombese <newtonombese1@gmail.com>
2022-10-31 14:03:32 +0300

    fixing the body and the navbar and now diving to responsiveness

--- src/App.js
+++ src/App.js
@@ -53,23 +53,25 @@ function App() {
     <div className="App">
     {/* <BrowserRouter> */}
     <Navbar user={user} setUser={setUser}/>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/service/:id' element={<Service />} />
-        <Route path='/receipts' element={<Receipts />} />
-        <Route path='/receipt/:id' element={<Receipt />} />
-        <Route path='/trackings' element={<Trackings />} />
-        <Route path='/orderform' element={<OrderForm />} />
-        <Route path='/ordercard' element={<OrderCard />} />
-        <Route path='/profile' element={<Profile />} />
-        {/* <Route path='/orders' element={<Orders />} /> */}
-        <Route path='/packagetrackings' element={<PackageTracking />} />
-        <Route path='/tracking/:id' element={<Tracking />} />
-        <Route path='/Login' element={<Login user={user} setUser={setUser} />} />
-        <Route path='/Signup' element={<Signup  />} />
-        <Route path='/Mapper' element={<Mapper  />} />
-    </Routes>
+    <div className="routes">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/service/:id' element={<Service />} />
+          <Route path='/receipts' element={<Receipts />} />
+          <Route path='/receipt/:id' element={<Receipt />} />
+          <Route path='/trackings' element={<Trackings />} />
+          <Route path='/orderform' element={<OrderForm />} />
+          <Route path='/ordercard' element={<OrderCard />} />
+          <Route path='/profile' element={<Profile />} />
+          {/* <Route path='/orders' element={<Orders />} /> */}
+          <Route path='/packagetrackings' element={<PackageTracking />} />
+          <Route path='/tracking/:id' element={<Tracking />} />
+          <Route path='/Login' element={<Login user={user} setUser={setUser} />} />
+          <Route path='/Signup' element={<Signup  />} />
+          <Route path='/Mapper' element={<Mapper  />} />
+        </Routes>
+    </div>
     <Footer />
   </div>
 
