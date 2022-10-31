c9b3e87e82b577bba3ef0c4a7c04e5647e5bf56b
Newton Ombese <newtonombese1@gmail.com>
2022-10-31 14:03:32 +0300

    fixing the body and the navbar and now diving to responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,10 @@ import axios from 'axios'
 import { useEffect } from 'react'
 // import bag from "../assets/svgs/bag.png"
 import { UserContext } from '../../hooks/useContext'
+import './Navbar.css'
 
 
 const Navbar = ({setUsers, user}) => {
-
-
 
     const handleLogout = () =>{
         fetch("users/signout",{
@@ -19,15 +18,13 @@ const Navbar = ({setUsers, user}) => {
         })
     }
 
-
-
   return (
     <div>
-          <nav class = "navbar navbar-expand-lg navbar-light bg-white  fixed-top">
+          <nav class = "navbar navbar-expand-lg navbar-light fixed-top navbarr">
         <div class = "container">
-            <a class = "navbar-brand d-flex justify-content-between align-items-center order-lg-0" href="/">
+            <a class = "navbar-brand d-flex justify-content-between align-items-center order-lg-0 spans" href="/">
 
-                <span class = "text-uppercase fw-lighter ms-2">Quick<span style={{color:"orange"}}>Fleet</span></span>
+                <span class = "text-uppercase fw-lighter ms-2 quick">Quick<span style={{color:"orange"}} class="fleet">Fleet</span></span>
             </a>
 
             <div class = "order-lg-2 nav-btns">
@@ -45,7 +42,7 @@ const Navbar = ({setUsers, user}) => {
                     <>
 
                 <button type = "button" class = "btn position-relative">
-                    <a href="/login" style={{color:"black"}}><i class="fa-solid fa-user"></i></a>
+                    <a href="/login" style={{color:"white"}}><i class="fa-solid fa-user"></i></a>
                 </button>
 
                     </>
@@ -60,10 +57,10 @@ const Navbar = ({setUsers, user}) => {
             <div class = "collapse navbar-collapse order-lg-1" id = "navMenu">
                 <ul class = "navbar-nav mx-auto text-center">
                     <li class = "nav-item px-2 py-2">
-                        <a class = "nav-link text-uppercase text-dark" href = "/">home</a>
+                        <a class = "nav-link text-uppercase text-light" href = "/">home</a>
                     </li>
                     <li class = "nav-item px-2 py-2">
-                        <a class = "nav-link text-uppercase text-dark" href = "/Service">Service</a>
+                        <a class = "nav-link text-uppercase text-light" href = "/Service">Service</a>
                     </li>
                 </ul>
             </div>
