5c40020ce5242c0fd7927caed5a659f6b9fe5760
loretta dhahabu <loreydjefwa@gmail.com>
2022-11-01 10:53:33 +0300

    resolve route error

--- src/components/OrderForm/OrderCard.js
+++ src/components/OrderForm/OrderCard.js
@@ -1,108 +1,106 @@
-import * as React from 'react';
-import {useEffect, useState} from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
 export default function MultiActionAreaCard() {
-    const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() =>{
-        fetch('http://127.0.0.1:3004/orders')
-        .then(res => res.json())
-        .then(data =>
-            setOrders(data))
-    }, [])
+  useEffect(() => {
+    fetch("/orders")
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
 
+  function handleDelete(id) {
+    fetch(`/orders/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
 
-    function handleDelete(id){
-        fetch(`http://localhost:3004/orders/${id}`,{
-            method: "DELETE",
-        })
+      .then(() => {
+        const deleting = orders.filter((table) => table.id !== id);
+        setOrders(deleting);
+        console.log("data");
+      })
+      .catch((err) => console.log(err));
+    alert("delete was successful");
 
-        .then((response) => response.json())
-
-        .then(() => {
-          const deleting = orders.filter((table) => table.id !== id);
-               setOrders(deleting);
-              console.log('data')
-              })
-        .catch((err) => console.log(err));    alert("delete was successful");
-
-
-
-        // .then((r)=>r.json())
-        // .then(()=>{
-        //     const deleting = orders.filter((order) => order.order !== order);
-        //     setOrders(deleting)
-        // })
-        // .catch((error) => console.log(error));
-        // alert("You've successfully canceled the order")
-    }
+    // .then((r)=>r.json())
+    // .then(()=>{
+    //     const deleting = orders.filter((order) => order.order !== order);
+    //     setOrders(deleting)
+    // })
+    // .catch((error) => console.log(error));
+    // alert("You've successfully canceled the order")
+  }
 
   return (
     <div>
-
-    {orders.map((table) => {
-        return(
-            <div class="mt-24">
+      {orders.map((table) => {
+        return (
+          <div class="mt-24">
             <br></br>
-                <br></br>
-                <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    alt="Delivery Van"
-                    image="https://i.pinimg.com/236x/98/23/1a/98231afda5095531306831dad557eef4.jpg"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Your Order Details
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        sender Name:
-                        {table.senderName}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Receiver Name:
-                        {table.receiverName}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Nature of Goods:
-                        {table.natureOfGoods}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Amount:
-                        {table.amountPaid}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Pickup:
-                        {table.pickup}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Destination:
-                        {table.destination}
-                    </Typography>
-                    {/* <Typography variant="body2" color="text.secondary">
+            <br></br>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  alt="Delivery Van"
+                  image="https://i.pinimg.com/236x/98/23/1a/98231afda5095531306831dad557eef4.jpg"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Your Order Details
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    sender Name:
+                    {table.senderName}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Receiver Name:
+                    {table.receiverName}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Nature of Goods:
+                    {table.natureOfGoods}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Amount:
+                    {table.amountPaid}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Pickup:
+                    {table.pickup}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Destination:
+                    {table.destination}
+                  </Typography>
+                  {/* <Typography variant="body2" color="text.secondary">
                         {table.senderName}
                     </Typography> */}
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <button class="bg-green-500 text-white font-bold py-2 px-4 rounded">
-                      EDIT ORDER
-                    </button>
-                    <button class="bg-red-500 text-white font-bold py-2 px-4 rounded" onClick={()=>handleDelete(table.id)}>
-                      CANCEL ORDER
-                    </button>
-                </CardActions>
-                </Card>
-            </div>
-        )
-    })}
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <button class="bg-green-500 text-white font-bold py-2 px-4 rounded">
+                  EDIT ORDER
+                </button>
+                <button
+                  class="bg-red-500 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => handleDelete(table.id)}
+                >
+                  CANCEL ORDER
+                </button>
+              </CardActions>
+            </Card>
+          </div>
+        );
+      })}
     </div>
   );
 }
