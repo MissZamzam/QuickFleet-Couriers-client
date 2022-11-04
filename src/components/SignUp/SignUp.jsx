5c863df883fb6c06cfdf02cc57f1c27168ab71d1
Newton Ombese <newtonombese1@gmail.com>
2022-11-04 04:17:55 +0300

    fixing the signup form|

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -14,10 +14,10 @@ export default function Registration() {
         password: "",
         password_confirmation:""
     });
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [telephone, setTelephone] = useState("")
-    const [location, setLocation] = useState("")
+    // const [firstName, setFirstName] = useState("")
+    // const [lastName, setLastName] = useState("")
+    // const [telephone, setTelephone] = useState("")
+    // const [location, setLocation] = useState("")
 
       const handleChange = (e) => {
         const { name, value } = e.target;
@@ -56,46 +56,41 @@ export default function Registration() {
             password_confirmation:""
         }))
 
-        fetch('/use_profile',{
-            method: 'POST',
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                firstName,
-                lastName,
-                location,
-                telephone,
-            }),
-        })
-        .then((r)=> r.json())
-        navigate('/profile')
+        // fetch('/use_profile',{
+        //     method: 'POST',
+        //     headers: {
+        //         "content-type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //         firstName,
+        //         lastName,
+        //         location,
+        //         telephone,
+        //     }),
+        // })
+        // .then((r)=> r.json())
+        navigate('/profileform')
     }
-
-    // const handleSubmit(e){
-
-    // }
 
     return (
         <div>
-            <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
+            <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50 signup-container">
                 <div>
                     <a href="/">
                         <h3 className="text-4xl font-bold text-600">
-                        <span>Sign</span><span className='fleet'>Up</span>
-
+                            <span className='quick'>Sign</span><span className='fleet'>Up</span>
                         </h3>
                     </a>
                 </div>
                 <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
                     <form onSubmit={handleSubmit}>
-                        <div class="flex flex-wrap -mx-3 mb-6">
+                        {/* <div class="flex flex-wrap -mx-3 mb-6">
                             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                                 First Name
                             </label>
                             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="" value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
-                            {/* <p class="text-red-500 text-xs italic">Please fill out this field.</p> */}
+                            <p class="text-red-500 text-xs italic">Please fill out this field.</p>
                             </div>
                             <div class="w-full md:w-1/2 px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
@@ -116,6 +111,23 @@ export default function Registration() {
                                 Location
                             </label>
                             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="" value={location} onChange={(e)=>setLocation(e.target.value)} />
+                            </div>
+                        </div> */}
+                        <div className="mt-4">
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700 undefined"
+                            >
+                                UserName
+                            </label>
+                            <div className="flex flex-col items-start">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    value={user.username}
+                                    onChange={(e)=>setUser.username(e.target.value)}
+                                />
                             </div>
                         </div>
                         <div className="mt-4">
