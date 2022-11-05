b4172def0d49d4d1e21184148bc19ce4f07e04fe
Zamzam <Zamzamhassan44@gmail.com>
2022-11-05 21:49:37 +0300

    update

--- src/auth/apiConfig.js
+++ src/auth/apiConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const base =
   process.env.NODE_ENV === "production"
     ? "hosting"
-    : "http://localhost:3004";
+    : "https://quickfleet-api.herokuapp.com";
 
 const api = axios.create({
   baseURL: base,
